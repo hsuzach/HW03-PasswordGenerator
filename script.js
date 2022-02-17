4c9fa18a16fe4897a36ceda4a8a68eabffe59b6c
hsuzach <hsuzach@gmail.com>
2022-02-17 13:09:57 -0800

    set criteria form and charset

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('generate').addEventListener('click', () => {
 
 })
+
+//create character set for password
+
+var charsetl = "abcdefghijklmnopqrstuvwxyz"
+var charsetu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "1234567890"
+var special = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
