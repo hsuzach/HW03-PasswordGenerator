6b5dc795f353dcdbee7296ef6166a8bc2e459624
hsuzach <hsuzach@gmail.com>
2022-02-18 11:02:50 -0800

    add event listener

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ document.getElementById('generate').addEventListener('click', () => {
 
 //create character set for password
 
-var charsetl = "abcdefghijklmnopqrstuvwxyz"
-var charsetu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numeric = "1234567890"
-var special = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var charsetl = "abcdefghijklmnopqrstuvwxyz";
+var charsetu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "1234567890";
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+var passEl = document.querySelector('#password');
+
+//display password function
+function dispPass() { passEl.textContent = 'Testing';
+
+}
+
+//adding button event listener
+
+document.getElementById('generate').addEventListener('click', 
+   function() { dispPass();
+  }
+);
